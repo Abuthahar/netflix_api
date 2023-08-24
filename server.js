28cd0615ef19b261219f192a5454b763788cc90a
Abu thahar <abuthaharg@hotmail.com>
2023-08-25 01:13:05 +0530

    Api for adding and removing liked movies

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/UserRoutes");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  // .connect("mongodb://localhost:27017/netflix", {
+  .connect("mongodb://127.0.0.1:27017/netflix", {
+    useNewUrlparser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connected...");
+  });
+
+app.use("/api/user", userRoutes);
+
+app.listen(5000, console.log("Server started..."));
